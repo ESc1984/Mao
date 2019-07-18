4a456b5bdd71944209cd0bc6fe18a8e2a849454e
Sommerseth <sommersl@sea-sommersl-m.ds.ad.adp.com>
2019-07-18 13:50:48 -0700

    removes the card player selected not first instance of the card in that players hand

--- javascript/game.js
+++ javascript/game.js
@@ -973,7 +973,7 @@ function playTurn() {
         let player = findPlayerIndexFromId();
         let cardIndex = -1;
         for(let i = 0; i < player.hand.length; i++){
-            if(player.hand[i].suit === selectedCard.charAt(0) && player.hand[i].value === selectedCard.charAt(1) /*&& player.hand[i].num.toString() === selectedCard.charAt(2)*/){
+            if(player.hand[i].suit === selectedCard.charAt(0) && player.hand[i].value === selectedCard.charAt(1) && player.hand[i].num.toString() === selectedCard.substring(2)){
                 cardIndex = i;
             }
         }
