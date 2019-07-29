78d7ac879b6b0b48d2525ba2d7dfc454bef694a1
Ellen Schaefer <schaefee@sea-schaefee-m.ds.ad.adp.com>
2019-07-29 08:20:14 -0700

    pushing to merge functionality

--- server.js
+++ server.js
@@ -20,29 +20,35 @@ const clients = new Set();
 let mode;
 
 function accept(req, res) {
-    if (req.url == '/ws' && req.headers.upgrade &&
-        req.headers.upgrade.toLowerCase() == 'websocket' &&
-        req.headers.connection.match(/\bupgrade\b/i)) {
-        wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-    } else if (req.url === '/') { // index.html
-        fs.createReadStream('mao.html').pipe(res);
-    } else if(req.url.match("\.js")){
-        let jsPath = path.join(__dirname, 'public', req.url);
-        let fileStream = fs.createReadStream(jsPath, "UTF-8");
-        res.writeHead(200, {"Content-Type": "text/javascript"});
-        fileStream.pipe(res);
-    } else if(req.url.match("\.css$")){
-        let cssPath = path.join(__dirname, 'public', req.url);
-        let fileStream = fs.createReadStream(cssPath, "UTF-8");
-        res.writeHead(200, {"Content-Type": "text/css"});
-        fileStream.pipe(res);
-    } else if(req.url.match("\.png$")){
-        let imagePath = path.join(__dirname, 'public', req.url);
-        let fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "image/png"});
-        fileStream.pipe(res);
-    } else { // page not found
-        res.writeHead(404);
+    try {
+        if (req.url == '/ws' && req.headers.upgrade &&
+            req.headers.upgrade.toLowerCase() == 'websocket' &&
+            req.headers.connection.match(/\bupgrade\b/i)) {
+            wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+        } else if (req.url === '/') { // index.html
+            fs.createReadStream('mao.html').pipe(res);
+        } else if (req.url.match("\.js")) {
+            let jsPath = path.join(__dirname, 'public', req.url);
+            let fileStream = fs.createReadStream(jsPath, "UTF-8");
+            res.writeHead(200, {"Content-Type": "text/javascript"});
+            fileStream.pipe(res);
+        } else if (req.url.match("\.css$")) {
+            let cssPath = path.join(__dirname, 'public', req.url);
+            let fileStream = fs.createReadStream(cssPath, "UTF-8");
+            res.writeHead(200, {"Content-Type": "text/css"});
+            fileStream.pipe(res);
+        } else if (req.url.match("\.png$")) {
+            let imagePath = path.join(__dirname, 'public', req.url);
+            let fileStream = fs.createReadStream(imagePath);
+            res.writeHead(200, {"Content-Type": "image/png"});
+            fileStream.pipe(res);
+        } else { // page not found
+            res.writeHead(404);
+            res.end();
+        }
+    } catch(error) {
+        console.error(error);
+        res.writeHead(500);
         res.end();
     }
 }
@@ -114,14 +120,21 @@ function onSocketConnect(ws) {
         console.log(`connection closed`);
         clients.delete(ws);
     });
+
+    ws.on('error', function(err) {
+        console.error(err);
+    })
 }
 
 if (!module.parent) {
     http.createServer(function(request, response){
-        //accept(request, response);
-        http.createServer(accept).listen(8080);
-        console.log("••• Listening on: " + ip.address() + ":8080 •••");
+        accept(request, response);
     }).listen(8080);
+    // http.createServer(function(request, response){
+    //     //accept(request, response);
+    //     http.createServer(accept).listen(8080);
+    // }).listen(8080);
+    console.log("••• Listening on: " + ip.address() + ":8080 •••");
 } else {
     // to embed into javascript.info
     let log = function () {};
